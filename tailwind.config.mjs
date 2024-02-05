5bbe283dcde5280bd91113667d91c2b58d51e302
Joe Linogao <linogaoj@tcd.ie>
2024-02-05 21:25:08 +0000

    started in progress page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const { nextui } = require("@nextui-org/react");
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['"Josefin Sans"', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				laidlawRed: "#d51f35",
+				laidlawTeal: "#23a698",
+				laidlawGrey: "#2d2926",
+				laidlawLightRed: "#fabbcb",
+				laidlawLightBlue: "#b1e4e3",
+				laidlawPurple: "#572c5f",
+			},
+		},
 	},
-	plugins: [],
-}
+	darkMode: "class",
+	plugins: [nextui()],
+};
