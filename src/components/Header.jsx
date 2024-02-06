becdc9672d9516cab260c99071d911eb2df1bbf3
Joe Linogao <linogaoj@tcd.ie>
2024-02-06 09:56:07 +0000

    fixed navabar issue and optimised logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import "./Navbar.css";
+import WhiteLogo from "./base64";
 
 export default function HeaderGlobal() {
 	return (
 		<header>
-			<nav class="bg-laidlawRed z-10" id="navbar-default">
-				<div class="w-full flex justify-center items-center p-6 gap-6 text-white">
+			<nav
+				class="bg-laidlawRed z-10 w-full flex justify-center"
+				id="navbar-default"
+			>
+				<div class="w-2/3 flex justify-center items-center p-6 gap-6 bg-laidlawRed text-white">
 					<div class="hidden w-full md:block md:w-auto">
 						<ul class="text-xl flex flex-row justify-center items-center">
 							<li>
@@ -29,9 +33,9 @@ export default function HeaderGlobal() {
 						class="flex items-center space-x-3 rtl:space-x-reverse"
 					>
 						<img
-							src="/lot_logo_landscape_white.png"
+							alt="lot_logo_landscape_white"
 							class="h-16"
-							alt="Laidlaw Opera Trust white logo"
+							src={WhiteLogo}
 						/>
 					</a>
 
