d127ed2b87fae43f68d130dea6098fb766cf2206
Joe Linogao <linogaoj@tcd.ie>
2024-02-13 13:58:15 +0000

    added royal opera house and new readme

--- src/components/GranteeIcon.tsx
+++ src/components/GranteeIcon.tsx
@@ -16,11 +16,14 @@ GranteeIcon.defaultProps = defaultProps
 export default function GranteeIcon(props: GranteeIcon) {
     return (
         <div className="flex flex-col items-center gap-2">
-            <img
-                src={props.imageSrc}
-                alt={props.alt}
-                className={`aspect-square w-36 rounded-full bg-white object-contain lg:w-48 ${props.padding}`}
-            />
+            <div id="image-wrapper" className="rounded-full bg-white">
+                <img
+                    src={props.imageSrc}
+                    alt={props.alt}
+                    className={`aspect-square w-36 object-contain lg:w-48 ${props.padding}`}
+                />
+            </div>
+
             <p className="text-wrap text-sm lg:text-xl">{props.name}</p>
         </div>
     )
