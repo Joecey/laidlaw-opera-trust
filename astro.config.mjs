a78ef597de4937a471cbde09e9d16eab68e00f82
Joe Linogao <linogaoj@tcd.ie>
2024-02-19 17:10:48 +0000

    fixed goals section

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from '@astrojs/tailwind'
 
 import node from '@astrojs/node'
 
-// https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind()],
     output: 'server',
