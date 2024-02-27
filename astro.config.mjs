64297ca64d7f924cf82cfbe909438237454a58eb
Joe Linogao <linogaoj@tcd.ie>
2024-02-27 19:16:47 +0000

    added partytown for third party scripts and GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import node from '@astrojs/node'
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind()],
+    site: 'https://laidlawopera.co.uk/',
+    integrations: [
+        react(),
+        tailwind(),
+        partytown({
+            config: {
+                forward: ['dataLayer.push'],
+            },
+        }),
+    ],
     output: 'server',
     adapter: node({
         mode: 'standalone',
